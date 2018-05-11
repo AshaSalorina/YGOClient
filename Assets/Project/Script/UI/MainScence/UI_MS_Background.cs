5386be74da2e20f6a22367271d64661e26ccda0f
AshaSalorina <he670004272@gmail.com>
2018-05-11 22:35:31 +0900

    调整加载方式，更新文件结构

--- Assets/Project/Script/UI/MainScence/UI_MS_Background.cs
+++ Assets/Project/Script/UI/MainScence/UI_MS_Background.cs
@@ -8,20 +8,30 @@ namespace Asha
 
     public class UI_MS_Background : MonoBehaviour
     {
+        static UI_MS_Background obj;
 
-        // Use this for initialization
         void Start()
+        {
+            obj = this;
+            Refresh();
+        }
+
+        /// <summary>
+        /// 刷新背景图
+        /// </summary>
+        public static void Refresh()
         {
             if (Setting.MS_Background_URL != null && Setting.MS_Background_URL != "")
             {
-                StartCoroutine(ImageHelper.LoadImage(gameObject, Setting.MS_Background_URL, ImageHelper.LoadImageType.WebOrLocal));
-                //StartCoroutine(ImageHelper.LoadImage(gameObject, "E:/YGOClient/Assets/Project/Resources/Images/Backgrounds/df1.jpg", ImageHelper.LoadImageType.WebOrLocal));
+                obj.StartCoroutine(ImageHelper.LoadImage(gameObject, Setting.MS_Background_URL, ImageHelper.LoadImageType.WebOrLocal));
             }
             else
             {
-                StartCoroutine(ImageHelper.LoadImage(gameObject, "Images/Backgrounds/df1", ImageHelper.LoadImageType.Resources));
+                obj.StartCoroutine(ImageHelper.LoadImage(gameObject, "Images/UI/Backgrounds/df1", ImageHelper.LoadImageType.Resources));
             }
         }
+
+
     }
 }
 

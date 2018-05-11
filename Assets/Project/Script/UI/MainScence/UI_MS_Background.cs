3e25c21522102c705e9c51a1476d5dda561449a6
AshaSalorina <he670004272@gmail.com>
2018-05-11 22:00:20 +0900

    修复资源加载失败的问题

--- Assets/Project/Script/UI/MainScence/UI_MS_Background.cs
+++ Assets/Project/Script/UI/MainScence/UI_MS_Background.cs
@@ -15,11 +15,11 @@ namespace Asha
             if (Setting.MS_Background_URL != null && Setting.MS_Background_URL != "")
             {
                 StartCoroutine(ImageHelper.LoadImage(gameObject, Setting.MS_Background_URL, ImageHelper.LoadImageType.WebOrLocal));
+                //StartCoroutine(ImageHelper.LoadImage(gameObject, "E:/YGOClient/Assets/Project/Resources/Images/Backgrounds/df1.jpg", ImageHelper.LoadImageType.WebOrLocal));
             }
             else
             {
-                // StartCoroutine(ImageHelper.LoadImage(gameObject, "/Images/Backgrounds/default.jpg", ImageHelper.LoadImageType.Resources));
-                gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("/Images/Backgrounds/default.jpg");
+                StartCoroutine(ImageHelper.LoadImage(gameObject, "Images/Backgrounds/df1", ImageHelper.LoadImageType.Resources));
             }
         }
     }

377901823fd2c32d7d06664df5fa1db2429906f8
AshaSalorina <he670004272@gmail.com>
2018-05-12 12:38:11 +0900

    脚本预留

---
+++ Assets/Project/Script/UI/MainScence/Menu/UI_MS_btnCardSet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_MS_plCardSet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

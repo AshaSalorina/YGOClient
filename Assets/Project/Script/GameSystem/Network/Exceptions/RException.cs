b39d135c9c19a67754f81b515e008c71da4a965f
Egan <1154610080@qq.com>
2018-05-15 23:47:45 +0800

    添加自定义异常类

---
+++ Assets/Project/Script/GameSystem/Network/Exceptions/RException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egan.Exceptions
+{
+    /// <summary>
+    /// 自定义异常
+    /// </summary>
+    public class RException : ApplicationException
+    {
+        public RException() { }
+
+        public RException(string message) : base(message) { }
+
+        public RException(string message, Exception inner) : base(message, inner) { }
+    }
+}

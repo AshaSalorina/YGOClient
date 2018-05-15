15d5a9fbba80e2be702232c37d618fa83f4b3e43
Egan <1154610080@qq.com>
2018-05-16 00:17:38 +0800

    添加服务器状态枚举

---
+++ Assets/Project/Script/GameSystem/Network/Tools/StatusCode.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Egan.Tools
+{
+    public enum StatusCode
+    {
+        /// <summary>
+        /// 正常
+        /// </summary>
+        OK = 2000,
+        /// <summary>
+        /// 房间密码错误
+        /// </summary>
+        Incorrect = 4316,
+        /// <summary>
+        /// Token无效
+        /// </summary>
+        InvalidToken = 4317,
+        /// <summary>
+        /// 房间已开始游戏
+        /// </summary>
+        Playing = 4381,
+        /// <summary>
+        /// 房客未准备
+        /// </summary>
+        Unprepared = 4382,
+        /// <summary>
+        /// 房间已解散
+        /// </summary>
+        Dimissed = 4383,
+        /// <summary>
+        /// 大厅已满
+        /// </summary>
+        FullLobby = 4391,
+        /// <summary>
+        /// 房间已满
+        /// </summary>
+        FullRoom = 4392
+    }
+}

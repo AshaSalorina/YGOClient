8f1ab8beefd579a20a4af25cab8514d320850f84
AshaSalorina <he670004272@gmail.com>
2018-05-17 22:46:09 +0900

    暂存部分游戏大厅的代码

--- Assets/Project/Script/UI/GameCenter/TestRoom.cs
+++ Assets/Project/Script/UI/GameCenter/TestRoom.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Egan.Cotrollers;
+using UnityEngine.UI;
 
 public class TestRoom : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Test();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,8 +19,24 @@ public class TestRoom : MonoBehaviour {
 
     void Test()
     {
-        NetworkWrapper nT = new NetworkWrapper();
-        nT.GetRooms();
+        try
+        {
+            NetworkWrapper nT = new NetworkWrapper();
+            var ls = nT.GetRooms();
+            /*
+            foreach (var item in ls)
+            {
+                item.Id
+            }
+            */
+            GameObject obj = gameObject.transform.Find("ID").gameObject;
+            obj.transform.GetComponent<Text>().text = ls[0].Id.ToString();
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e);
+        }
+
     }
 
 }
--- Assets/Project/Script/UI/GameCenter/UI_GC_SVViewSize.cs
+++ Assets/Project/Script/UI/GameCenter/UI_GC_SVViewSize.cs
@@ -1,19 +1,27 @@
-﻿using System.Collections;
+﻿using Egan.Cotrollers;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace Asha
 {
     public class UI_GC_SVViewSize : MonoBehaviour
     {
-
+        ScrollRect sR;
         // Use this for initialization
         void Start()
         {
+            sR = gameObject.GetComponent<ScrollRect>();
             Refresh();
+            StartCoroutine(RefreshRoomList());
         }
 
+        private void OnDisable()
+        {
+            StopCoroutine(RefreshRoomList());
+        }
         /// <summary>
         /// 刷新房间控件的大小
         /// </summary>
@@ -22,6 +30,44 @@ namespace Asha
             int ct = gameObject.transform.childCount;
             Vector2 v2 = gameObject.GetComponent<RectTransform>().sizeDelta;
             gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(v2.x, ct * 105);
+        }
+
+
+        public IEnumerator RefreshRoomList()
+        {
+            while (true)
+            {
+                
+                //移除旧的列表
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    Destroy(transform.GetChild(i).gameObject);
+                }
+                try
+                {
+                    //通过NetWorkWrapper获取房间列表
+                    var nT = new NetworkWrapper();
+                    var ls = nT.GetRooms();
+                    foreach (var item in ls)
+                    {
+                        GameObject obj = InstantiateHelper.InsObj(Resources.Load<GameObject>(@"Prefabs\UI\GameCenter\room"), gameObject);
+                        obj.transform.Find("ID").GetComponent<Text>().text = item.Id.ToString();
+                        obj.name = item.Name;
+                        obj.transform.Find("Name").GetComponent<Text>().text = item.Name;
+                        obj.transform.Find("Host").GetComponent<Text>().text = item.Host.Name;
+                        obj.transform.Find("Desc").GetComponent<Text>().text = item.Desc;
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    //todo：删除debug
+                    Debug.Log(e.ToString());
+                    //throw;
+                }
+                Refresh();
+                yield return new WaitForSeconds(30f);
+            }
+
         }
 
 

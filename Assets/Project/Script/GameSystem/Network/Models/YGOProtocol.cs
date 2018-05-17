5c4ef7caf526c95bc30e9e72af76b3a5659ee10b
Egan <1154610080@qq.com>
2018-05-18 00:36:53 +0800

    添加YGOP协议类和消息类型枚举

---
+++ Assets/Project/Script/GameSystem/Network/Models/YGOProtocol.cs
@@ -0,0 +1,15 @@
+﻿using Egan.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egan.Models
+{
+    public class YGOProtocol
+    {
+        private MessageType type;
+
+    }
+}

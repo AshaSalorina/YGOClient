3b6bc5fd43f1c4280fef8430543296c68c53f79b
AshaSalorina <he670004272@gmail.com>
2018-05-22 00:08:58 +0900

    暂存

---
+++ Assets/Project/Script/GameSystem/Network/Models/IObserver.cs
@@ -0,0 +1,37 @@
+﻿using Egan.Models;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public interface IObserver{
+    int MessageCount { get; }
+    DataPacket SendDataPacket();
+}
+
+public class Test : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Fun(new ThreadStart(TA));
+    }
+
+    public void Fun(ThreadStart start)
+    {
+        StartCoroutine(GetInfo());
+    }
+    public void TA()
+    {
+
+    }
+    IEnumerator GetInfo(DataPacket dp)
+    {
+        while (true)
+        {
+
+            yield return TA();
+        }
+        
+    }
+}

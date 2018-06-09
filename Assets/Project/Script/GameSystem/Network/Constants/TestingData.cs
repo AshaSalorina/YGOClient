28dae824764659a5c821dc465f3bce756c6cd393
Egan <1154610080@qq.com>
2018-06-09 09:33:27 +0800

    添加测试数据类

---
+++ Assets/Project/Script/GameSystem/Network/Constants/TestingData.cs
@@ -0,0 +1,23 @@
+﻿using Egan.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network.Constants
+{
+    /// <summary>
+    /// 数据测试类
+    /// </summary>
+    public class TestingData
+    {
+        public static int id = 0;
+        public static bool isHost;
+        public static NetworkClient client;
+
+        public static bool isSP_Our = false;
+        public static bool isSP_Opp = false;
+       
+    }
+}
